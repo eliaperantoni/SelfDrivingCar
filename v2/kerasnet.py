089e51f2b3292f273806813b673b7daf5ab9f62f
Elia Perantoni <perantonielia0@gmail.com>
2018-01-10 15:37:53 +0100

    Preparing for keras net

---
+++ v2/kerasnet.py
@@ -0,0 +1,18 @@
+import keras
+from keras.datasets import mnist
+from keras.layers import Dense, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.models import Sequential
+import matplotlib.pylab as plt
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+def kerasnet(width, height, learning_rate):
+    model = Sequential()
+    model.add()
