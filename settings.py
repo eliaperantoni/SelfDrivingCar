a80738f050b2d956b3c865853aad59301acd3e70
Elia Perantoni <perantonielia0@gmail.com>
2018-02-10 15:33:33 +0100

    Modified settings

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ settings = {
     "BALANCE_MODE": 'RESAMPLE',
     "DEFAULT_TRAIN_FILE_DIRECTORY": 'train_data/raw/',
     "DEFAULT_TRAIN_FILE": 'train_data/raw/training_data_{}.npy',
-    "DEFAULT_TRAIN_FILE_PROCESSED_DIRECTORY": 'train_data/raw/',
+    "DEFAULT_TRAIN_FILE_PROCESSED_DIRECTORY": 'train_data/processed/',
     "DEFAULT_TRAIN_FILE_PROCESSED_NORM": 'train_data/processed/training_data_processed_norm_{}.npy',
     "DEFAULT_TRAIN_FILE_PROCESSED_MIRR": 'train_data/processed/training_data_processed_mirr_{}.npy',
     "LEARNING_RATE": 1e-3,
@@ -14,5 +14,5 @@ settings = {
     "VALIDATION_SPLIT": 0.1,
     "BATCH_SIZE": 100,
     "DEFAULT_MODEL_FILE": 'saved_models/env_v3.h5',
-    "TARGET_SPEED": 50
+    "TARGET_SPEED": 40
 }
